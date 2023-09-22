db112efa77daafe1c0a68322c0a60f43b0d7c710
Marco Giammari <marcogiammari@gmail.com>
2023-09-22 10:55:56 +0200

    factory & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Book::factory(33)->create()->each(function ($book) {
+
+            // per ogni libro crea da 5 a 30 recensioni
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                // sovrascrive i dati chiamando il metodo custom  
+                ->good()
+                // crea un'associazione con il libro settando la colonna "book_id"
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+
+            // per ogni libro crea da 5 a 30 recensioni
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                // sovrascrive i dati chiamando il metodo custom  
+                ->average()
+                // crea un'associazione con il libro settando la colonna "book_id"
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+
+            // per ogni libro crea da 5 a 30 recensioni
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+                // sovrascrive i dati chiamando il metodo custom 
+                ->bad()
+                // crea un'associazione con il libro settando la colonna "book_id"
+                ->for($book)
+                ->create();
+        });
     }
 }
