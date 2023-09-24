1200b3ad53075d627472af09d42261efdd684b5f
Marco Giammari <marcogiammari@gmail.com>
2023-09-24 10:16:07 +0200

    popular or highest rated - the view

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index']);
 
 Route::resource('books', BookController::class);
